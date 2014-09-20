4066cc1dbfe8fe485dd3e622ba8d651780b866bb
dikshanangia <dikshanangia@Dikshas-MacBook-Air.local>
2014-09-20 23:11:03 +0530

    Inversematrix
    Calculates an inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions take a matrix as an argument to the
+##  matrix and calculate the inverse of the matrix
 
-## Write a short comment describing this function
+
+## This function creates a matrix and assigns default values to the variables
 
 makeCacheMatrix <- function(x = matrix()) {
+{
+  m <- NULL
+  set <- function(y) 
+    {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
 
 }
 
 
-## Write a short comment describing this function
+## This function takes the value of the matrix from the makeCacheMatrix function and calculates the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
